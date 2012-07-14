8a49de69848c5bb6fc0984a2ad89e80b341c6e8b
E2Blue <sebastian.rosengren@e2blue.com>
2012-07-14 15:59:44 +0200

    Sample layout, fluid, slider,colors
    Added a simple sample layout showing how to use the framework.
    
    Implemented fluid container classes.
    
    Reimplemented the slider. The functionality is the same but it is
    much more robust and can handle more than one slider now.
    
    Added metro-colors.css, these are the standard colors for Windowsphone
    which are suited for a metro layout

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Metro Web UI
 
 This project is meant to provide a simple to use framework
 for building metro UI style web applications.
+
+
+Usage
+==========
+
+Immediate child divs and li elements of tiles are automaticaly sized to the tile, this is to keep a nice layout for sliding
--- scripts/metro.js
+++ scripts/metro.js
@@ -1,13 +1,20 @@
 
-/* Perform a sliding effect of the elements children with class="sliding-js" */
-$.fn.metroSlide = function(){
-	that = $(this);
-	if(that.children.length > 1){
-		setInterval(function(){
-			that.children(':nth-child(2)').show();
-			that.children(':first').slideUp(700,function(){
-				that.append($(this));
-			});
-		},2000);
+/* Perform a sliding effect of the elements children */
+(function($){
+	jQuery.fn.metroSlide = function(){
+		this.each(function(){
+			that = $(this);
+			if(that.children.length > 1){
+				(function(that){
+					setInterval(function(){
+						that.children(':nth-child(2)').show();
+						that.children(':first').slideUp(830,function(){
+							that.append($(this));
+						});
+					},2000);
+				})(that);
+			}
+		});
+		return this;
 	}
-}
+})(jQuery);
